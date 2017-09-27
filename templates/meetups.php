284178ed8dbcaf52ce2bfde1a4a3ba66f76f2193
Aaron Bergstrom <abergstr@undhpcdigitalag.und.edu>
2017-09-27 13:49:50 -0500

    Updates to the meetup group pages.

--- templates/meetups.php
+++ templates/meetups.php
@@ -1,22 +1,48 @@
 <?php
-	$mupurl = "https://www.meetup.com/pro/producers";
+//	require_once('../php_scripts/simple_html_dom.php');
+//	$mupurl = "https://www.meetup.com/pro/producers";
 	
-	$options = array('http' => array('header' => 'Content-Type: text/html; charset=utf-8','method' => 'GET'));
- 	$context = stream_context_create($options);
-	$response = file_get_contents($mupurl, false, $context);
+//	$options = array('http' => array('header' => 'Content-Type: text/html; charset=utf-8','method' => 'GET'));
+// 	$context = stream_context_create($options);
+//	$page = file_get_html($mupurl);
 
-	$chop = explode('Meetup Groups Nearby', $response);
+//	$response = file_get_contents($mupurl);
+
+//	$chop = explode('groups', $response);
+
+//	$grpChop = explode('<h3', $chop[1]);
+/*
 	$bkchop = explode('<ul class="list">', $chop[1]);
 	$endchop = explode('</ul>', $bkchop[1]);
 
 	$lichop = explode('</li>', $endchop[0]);
+*/
+//	$cnt = count($grpChop);
 
-	$cnt = count($lichop)-1;
-
+	$mLink = array("https://www.meetup.com/ND-RR-Valley-North-UAS-Digital-Precision-Ag-Big-Data",
+			"https://www.meetup.com/ND-RRValley-UAS-Precision-Agriculture",
+			"https://www.meetup.com/Data-Science-and-Big-Data-South-Dakota",
+			"https://www.meetup.com/Nebraska-UAS-Precision-Agriculture-and-Big-Data-Meetup",
+			"https://www.meetup.com/UAS-Digital-and-Precision-Ag-and-Big-Data-Meetup",
+			"https://www.meetup.com/Rolla-Big-Data-Meetup");
+	$mTitle= array("ND RR Valley (North) : UAS Digital/Precision Ag & Big Data",
+			"ND RR Valley (South) : UAS Digital/Precision Ag & Big Data",
+			"Data Science and Big Data - South Dakota",
+			"Nebraska - UAS Precision Agriculture and Big Data Meetup",
+			"Kansas UAS Digital/Precision Ag and Big Data Meetup",
+			"Missouri UAS Digital/Precision Ag and Big Data Meetup");
+	$mLoc = array("Grand Forks, ND",
+			"Fargo, ND",
+			"Sioux Falls, SD",
+			"Lincoln, NE",
+			"Manhattan, KS",
+			"Rolla, MO");
 	$html = "";
-
-	for($i=0; $i<$cnt; $i++)
+	for($i=0; $i<6; $i++)
 	{
+//		$subGrp = explode('</p>',$grpChop[$i]);
+//		$reconst = "<h3".$subGrp[0]."</p>";
+/*
 		$gtrchop = explode('>', $lichop[$i]);
 		
 		$urlchop = explode('"', $gtrchop[3]);
@@ -28,16 +54,16 @@
 		$chapnme = trim($chpchop[0], "\n");
 		$chapcnt = trim($memchop[0], "\n");
 		$chaploc = trim($locchop[0], "\n");
-
+*/
 		$html .= "<div style='margin-top: 5px; margin-bottom: 5px;'><table><tr><td style='padding: 5px; width: 32px; vertical-align: top; align: left;'>";
 		$html .= "<img alt='UASPSE Icon' style='height: 32px;' src='img/uaspse-48.png' /></td>";
 		$html .= "<td style='padding: 5px; vertical-align: top; align: left;'>";
-		$html .= "<p style='font-weight: bold;'>".$chapnme."<br>";
-		$html .= "Location: ".$chaploc.", ".$chapcnt."<br><a href='".$chapurl."' target='meetups'>";
+		$html .= "<p style='font-weight: bold;'>".$mTitle[$i]."<br>";
+		$html .= "Location: ".$mLoc[$i]."<br><a href='".$mLink[$i]."' target='meetups'>";
 		$html .= "Group Meetup Page</a></p></td></tr></table></div>";
+
 	}
 
 	echo $html;
-//	echo "<ul class='list'>".$endchop[0]."</ul>";
 
 ?>
